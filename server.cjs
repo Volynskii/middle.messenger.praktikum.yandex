5019f46acfde1a5d5094c9e3f6dfc6aff874f16c
Anton Wolynski <50911673+Volynskii@users.noreply.github.com>
2023-11-09 12:46:30 +0300

    feature/commit;

--- server.cjs
+++ server.cjs
@@ -9,29 +9,31 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.get('/auth', (req, res) => {
+    res.sendFile('src/pages/auth/auth.html', { root: path.join(__dirname, 'dist') });
+});
+
 app.get('/registration', (req, res) => {
     res.sendFile('src/pages/registration/registration.html', { root: path.join(__dirname, 'dist') });
 });
 
+app.get('/edit', (req, res) => {
+    res.sendFile('src/pages/edit/edit.html', { root: path.join(__dirname, 'dist') });
+});
+
 app.get('/chat', (req, res) => {
-    res.sendFile('src/pages/chatWindow/chatWindow.html', { root: path.join(__dirname, 'dist') });
+    res.sendFile('src/pages/chat/chat.html', { root: path.join(__dirname, 'dist') });
 });
 
-app.get('/settings', (req, res) => {
-    res.sendFile('src/pages/settingsPage/settingsPage.html', { root: path.join(__dirname, 'dist') });
+app.get('/notFound', (req, res) => {
+    res.sendFile('src/pages/404Page/404Page.html', { root: path.join(__dirname, 'dist') });
 });
 
-app.get('/500', (req, res) => {
+app.get('/serverError', (req, res) => {
     res.sendFile('src/pages/500Page/500Page.html', { root: path.join(__dirname, 'dist') });
 });
 
-app.get('/404', (req, res) => {
-    res.sendFile('src/pages/404Page/404Page.html', { root: path.join(__dirname, 'dist') });
-});
-app.get('/test', (req, res) => {
-    res.sendFile('src/pages/404Page/404Page.html', { root: path.join(__dirname, 'dist') });
-});
 
 app.listen(port, () => {
-    console.log(`Listening ${port}`);
+    console.log(`Server is Listening on port ${port}`);
 });
