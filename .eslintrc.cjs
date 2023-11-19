be0226dce7cd6a393661f44e7ccbbf9bc4f80e22
Anton <a.volynskiy@brl.ru>
2023-11-19 17:40:56 +0300

    feature/eslint ignore;

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,33 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
     "overrides": [
         {
-            "files": ["main.js"],
-            "excludedFiles": ["main.js"]
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
         }
     ],
-    extends: ['airbnb-base', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended'],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: './tsconfig.json',
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    rules: {
-        'max-len': [2, { code: 180 }],
-        'no-underscore-dangle': [2, { allowAfterThis: true }],
-        'no-console': 'warn',
-        'padding-line-between-statements': [2, { blankLine: 'always', prev: '*', next: 'return' }],
-        'import/prefer-default-export': 'off',
-        'class-methods-use-this': 'off',
-        'no-restricted-exports': 'off',
-        'no-param-reassign': 'off',
-        'import/no-absolute-path': 'off',
-        'arrow-body-style': 'off',
-        'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
-        indent: 'off',
-        '@typescript-eslint/indent': 'off',
-        'linebreak-style': 'off',
-    },
-};
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
