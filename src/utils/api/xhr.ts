4ab7c517684cffbe6fe55ca55bfaa7d87e819807
Anton <a.volynskiy@brl.ru>
2023-12-16 17:00:05 +0300

    hotfix/block;

--- src/utils/api/xhr.ts
+++ src/utils/api/xhr.ts
@@ -1,20 +1,20 @@
 const enum HttpMethodsEnum {
-        GET = 'GET',
-        POST = 'POST',
-        PUT = 'PUT',
-        DELETE = 'DELETE',
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
 }
 
 export type HttpRequestOptions = {
     method?: HttpMethodsEnum;
-    data?: unknown;
+    data?: any;
     headers?: Record<string, string>;
     timeout?: number;
 };
 
 type HTTPMethod = (url: string, options?: HttpRequestOptions) => Promise<XMLHttpRequest>;
 
-function queryStringify(data: Record<string, unknown>) {
+function queryStringify(data: Record<string, any>) {
     const queryKeys = Object.keys(data);
 
     return queryKeys.reduce((acc, key, index) => `${acc}${key}=${data[key]}${index < queryKeys.length - 1 ? '&' : ''}`, '?');
@@ -39,33 +39,36 @@ export default class ApiService {
 
     request = (
         url: string,
-    options: HttpRequestOptions = { method: HttpMethodsEnum.GET },
-    timeout: number = 10_000,
-): Promise<XMLHttpRequest> => {
-    const { headers = {}, method, data } = options;
+        options: HttpRequestOptions = { method: HttpMethodsEnum.GET },
+        timeout: number = 10_000,
+    ): Promise<XMLHttpRequest> => {
+        const { headers = {}, method, data } = options;
 
-return new Promise((resolve, reject) => {
-    const xhr: XMLHttpRequest = new XMLHttpRequest();
-    // timeout
-    xhr.timeout = timeout;
-    xhr.open(
-        method!,
-        (method === HttpMethodsEnum.GET) && data
-            ? `${url}${queryStringify(data)}`
-            : url,
-);
+        return new Promise((resolve, reject) => {
+            const xhr: XMLHttpRequest = new XMLHttpRequest();
+            // timeout
+            xhr.timeout = timeout;
+            xhr.open(
+                method!,
+                (method === HttpMethodsEnum.GET) && data
+                    ? `${url}${queryStringify(data)}`
+                    : url,
+            );
 
-    Object.keys(headers).forEach((key) => xhr.setRequestHeader(key, headers[key]));
-    xhr.onload = () => resolve(xhr);
-    xhr.onabort = reject;
-    xhr.onerror = reject;
-    xhr.ontimeout = reject;
+            // headers
+            Object.keys(headers).forEach((key) => xhr.setRequestHeader(key, headers[key]));
 
-    if (method === HttpMethodsEnum.GET || !data) {
-        xhr.send();
-    } else {
-        xhr.send(data);
-    }
-});
-};
+            xhr.onload = () => resolve(xhr);
+
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+
+            if (method === HttpMethodsEnum.GET || !data) {
+                xhr.send();
+            } else {
+                xhr.send(data);
+            }
+        });
+    };
 }
